e6e94a458f411939711b27c30d8fb29646e0b493
matej-kotrba <matejkotrba.skola@gmail.com>
2024-08-06 15:26:31 +0200

    update: menu button design

--- src/lib/components/main-menu/menu-button.svelte
+++ src/lib/components/main-menu/menu-button.svelte
@@ -12,23 +12,71 @@
 	let { children, href, onClick, class: className }: Props = $props();
 </script>
 
-<div class="relative">
+<a {href} onclick={onClick} class="btn-container relative isolate cursor-pointer text-center">
+	<div class="hover-effect"></div>
 	<div class="borders">
 		<div></div>
 		<div></div>
 	</div>
-	<a {href} onclick={onClick} class={twMerge('px-8 py-4 text-6xl', className)}>
+	<div class={twMerge('content px-8 py-4 text-6xl', className)}>
 		{@render children()}
-	</a>
-</div>
+	</div>
+</a>
 
 <style>
+	.hover-effect {
+		position: absolute;
+		width: 100%;
+		height: 100%;
+		left: 0;
+		top: 0;
+		background-color: white;
+		z-index: -99;
+		transform: scaleX(0);
+		transform-origin: left;
+		opacity: 0;
+		transition:
+			transform 0.4s ease,
+			opacity 0.4s;
+		transition-delay: 0s;
+	}
+
+	.btn-container:hover .hover-effect {
+		transform: scaleX(1);
+		opacity: 1;
+		transition-delay: 0.4s;
+	}
+
+	.content {
+		color: white;
+		transition: color 0.3s;
+	}
+
+	.btn-container:hover .content {
+		color: black;
+		transition-delay: 0.4s;
+	}
+
 	.borders > div {
 		position: absolute;
 		width: 100%;
 		height: 2px;
-		background-color: var(--color-slate-800);
+		background-color: white;
 		right: 0;
 		transform-origin: right;
+		transform: scaleX(0);
+		transition: transform 0.3s cubic-bezier(0.81, 0.285, 0.805, 0.615);
+	}
+
+	.borders > div:nth-of-type(1) {
+		top: 0;
+	}
+
+	.borders > div:nth-of-type(2) {
+		bottom: 0;
+	}
+
+	.btn-container:hover .borders > div {
+		transform: scaleX(1);
 	}
 </style>
--- src/lib/components/main-menu/menu.svelte
+++ src/lib/components/main-menu/menu.svelte
@@ -5,7 +5,7 @@
 <div class="grid h-screen items-center">
 	<div class="h-72 w-full bg-slate-800"></div>
 
-	<section class="flex flex-col items-center self-start">
+	<section class="mx-auto flex w-fit flex-col items-stretch gap-2 self-start">
 		<MenuButton>New game</MenuButton>
 		<MenuButton>Levels</MenuButton>
 		<MenuButton>About</MenuButton>
