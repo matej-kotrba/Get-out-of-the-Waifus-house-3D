10ce7566a0166948ea7c57b3ef771052f0dfe5e4
matej-kotrba <matejkotrba.skola@gmail.com>
2024-08-27 21:26:20 +0200

    update: animation and bg to inventory selected item

--- src/lib/components/game/ingame-ui/inventory.svelte
+++ src/lib/components/game/ingame-ui/inventory.svelte
@@ -6,13 +6,14 @@
 {#if player.inventory?.selectedSlot !== undefined}
 	<div
 		class="fixed bottom-0 right-0 flex items-end"
-		style="width: {player.inventory['items'].length * 75}px;"
+		style="width: {player.inventory['items'].length * 70}px;"
 	>
 		{#each player.inventory['items'] as slot}
 			{#if slot.id}
 				{@const record = inventoryItemsRecord[slot.id]}
 				<div
-					class="basis-0"
+					class="basis-0 duration-100"
+					class:gradient-effect={player.inventory.selectedSlot === slot.slotId}
 					style="flex-grow: {player.inventory.selectedSlot === slot.slotId
 						? '2'
 						: '1'};"
@@ -23,3 +24,13 @@
 		{/each}
 	</div>
 {/if}
+
+<style>
+	.gradient-effect {
+		background-image: linear-gradient(
+			180deg,
+			transparent,
+			rgba(244, 201, 7, 0.852) 100%
+		);
+	}
+</style>
